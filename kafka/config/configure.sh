70edde01208ac1e7b4549e17289095e67a797160
Francois Langelier <francois.langelier@mate1inc.com>
2015-08-06 16:47:40 -0400

    update conf

--- kafka/config/configure.sh
+++ kafka/config/configure.sh
@@ -3,3 +3,10 @@
 cp server.properties /kafka
 echo "YO"
 
+#sed -i "s#.\?broker.id=.*#broker.id=$broker_id#" /etc/kafka/server.properties 
+#sed -i "s#.\?zookeeper.connect=.*#zookeeper.connect=$zkconnect#" /etc/kafka/server.properties 
+#sed -i "s#.\?advertised.host.name=.*#advertised.host.name=`hostname -f`#" /etc/kafka/server.properties 
+#sed -i "s#.\?num.partitions=.*#num.partitions=$partition#" /etc/kafka/server.properties 
+#sed -i "s#.\?default.replication.factor=.*#default.replication.factor=$replication#" /etc/kafka/server.properties 
+
+
